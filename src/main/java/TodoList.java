0ad5f2f301205863e1073d263502a936cc9b020e
sreesubbash <vostrosubbash97@hotmail.com>
2019-08-21 08:26:54 +0800

    Level 4 done

--- src/main/java/TodoList.java
+++ src/main/java/TodoList.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,19 +11,37 @@ public class TodoList {
     }
 
     public void action(String move) {
-        String[] moveSplit = move.split(" ");
+        List<String> moveSplit = new LinkedList<>(Arrays.asList(move.split(" ")));
+        String type = moveSplit.get(0);
+        moveSplit.remove(0);
+        String task = String.join(" ", moveSplit);
         if (move.equals("list")) {
             printList();
-        } else if (moveSplit[0].equals("done")) {
-            Task current = todos.get(Integer.parseInt(moveSplit[1])-1);
+        } else if (type.equals("done")) {
+            Task current = todos.get(Integer.parseInt(moveSplit.get(0)) - 1);
             current.markAsDone();
             System.out.println("Nice! I've marked this task as done: \n" + current);
-
-
         } else {
-            todos.add(new Task(move));
-            System.out.println("added: " + move);
+            addTask(type, task);
         }
+    }
+
+    private void addTask(String type, String task) {
+        Task current;
+        if (type.equals("todo")) {
+            current = new ToDo(task);
+        } else if (type.equals("deadline")) {
+            List<String> temp  =  new LinkedList<>(Arrays.asList(task.split(" /by ")));
+            current = new Deadline(temp.get(0), temp.get(1));
+        } else if (type.equals("event")) {
+            List<String> temp  =  new LinkedList<>(Arrays.asList(task.split(" /at ")));
+            current = new Event(temp.get(0), temp.get(1));
+        } else {
+            return;
+        }
+        todos.add(current);
+        System.out.println("Got it. I've added this task: \n" + current
+            + "\nNow you have " + todos.size() + " tasks in the list.");
     }
 
     private void printList() {
